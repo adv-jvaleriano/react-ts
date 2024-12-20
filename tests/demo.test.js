a4eaae992da9551837840436ed4987610a1c3546
adv-jvaleriano <juan.valeriano@adviters.com>
2024-12-20 00:52:32 -0300

    test: correct merge

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -2,3 +2,12 @@ test('Suma dos números pares y obtiene un resultado par', () => {
   const total = 1 + 1;
   expect(total % 2).toBe(0);
 });
+
+test('Suma dos números y obtiene un resultado impar', () => {
+  
+  const number1 = 2;
+  const number2 = 1;
+  const total = number1 + number2;
+  expect(total % 2).not.toBe(0);
+});
+
