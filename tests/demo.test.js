a0875394c62f2536390362c5552e57d9efb597b7
adv-jvaleriano <133686378+adv-jvaleriano@users.noreply.github.com>
2024-12-20 00:49:38 -0300

    Update demo.test.js

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,7 +1,4 @@
 test('Suma dos números pares y obtiene un resultado par', () => {
-  
-  const number1 = 2;
-  const number2 = 2;
-  const total = number1 + number2;
+  const total = 1 + 1;
   expect(total % 2).toBe(0);
 });
